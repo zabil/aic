4ca2bc169fc5669e07abbbfc526dd000dd1d60de
Zabil Cheriya Maliackal <zabilcm@gmail.com>
2024-05-01 16:26:24 +0100

    Initial version of streaming api

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,35 @@
 import OpenAI from "openai";
+import ora from "ora";
 
 async function main() {
   const apiKey = process.env.OPENAI_API_KEY;
   if (!apiKey) {
     console.log(
-
       "The OPENAI_API_KEY environment variable is not defined. Please set it and try again.",
-    ,
     );
     process.exit(1);
   }
 
   const openai = new OpenAI({ apiKey });
+  if (process.argv.length <= 2) {
+    console.log("Please provide a prompt and try again.");
+    process.exit(1);
+  }
+
+  const content = process.argv.slice(2).join(" ");
+
+  const spinner = ora().start();
   const stream = await openai.chat.completions.create({
     model: "gpt-4",
-    messages: [{ role: "user", content: "Say this is a test" }],
+    messages: [
+      {
+        role: "user",
+        content,
+      },
+    ],
     stream: true,
   });
+  spinner.stop();
   for await (const part of stream) {
     process.stdout.write(part.choices[0]?.delta?.content || "");
   }
